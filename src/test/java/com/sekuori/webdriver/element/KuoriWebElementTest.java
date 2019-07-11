b4939e3060acc51395069bebdc30b4af77fe661c
Oleh Hryshchenko <tendio94@gmail.com>
2019-07-11 21:13:56 +0300

    test: added dummy test for KuoriWebElement default implementation

--- src/test/java/com/sekuori/webdriver/element/KuoriWebElementTest.java
+++ src/test/java/com/sekuori/webdriver/element/KuoriWebElementTest.java
@@ -1,5 +1,11 @@
-import static org.junit.Assert.*;
+package com.sekuori.webdriver.element;
+
+import org.junit.Test;
 
 public class KuoriWebElementTest {
-
+    @Test
+    public void testWrappedWebElementCreation() {
+        KuoriWebElement element = new KuoriWebElement();
+        element.get(KuoriWebElement.class, element);
+    }
 }
