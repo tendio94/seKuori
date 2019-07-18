071ca3a0f0503c03368f88a31f46dd92621281c5
Oleh HRYSHCHENKO <oleh.hryshchenko-external@rbinternational.com>
2019-07-18 17:44:25 +0300

    test: added proper KuoriWebElement initialization - with type

--- src/test/java/com/sekuori/webdriver/element/KuoriWebElementTest.java
+++ src/test/java/com/sekuori/webdriver/element/KuoriWebElementTest.java
@@ -9,13 +9,13 @@ public class KuoriWebElementTest {
 
     @Test(expected = SearchContextNotSetException.class)
     public void testGetElementThrowsExceptionForNullSearchContext() {
-        KuoriWebElement element = new KuoriWebElement();
+        KuoriWebElement<KuoriWebElement> element = new KuoriWebElement<>();
         element.get(KuoriWebElement.class, null);
     }
 
     @Test
     public void testGetElementFindsWebElementByCustomConfiguration() {
-        KuoriWebElement element = new KuoriWebElement();
+        KuoriWebElement<DummyElement> element = new KuoriWebElement<>();
         getDriver().get("https://www.google.com/");
 
         String expectedTagName = "input";
