3d0dcac32a00e8b1b69b67d4fbfdaf5624a9464d
Oleh HRYSHCHENKO <oleh.hryshchenko-external@rbinternational.com>
2019-07-18 17:27:44 +0300

    misc: removed redundant @NonNls import

--- src/main/java/com/sekuori/webdriver/element/SearchContextNotSetException.java
+++ src/main/java/com/sekuori/webdriver/element/SearchContextNotSetException.java
@@ -1,12 +1,10 @@
 package com.sekuori.webdriver.element;
 
-import org.jetbrains.annotations.NonNls;
-
 public class SearchContextNotSetException extends RuntimeException {
     private static final long serialVersionUID = -4512905395583001098L;
     private static final String DEFAULT_EXCEPTION_CAUSE = "Search context of the element provided isn`t set";
 
-    public SearchContextNotSetException(@NonNls String message) {
+    public SearchContextNotSetException(String message) {
         super(message);
     }
 
